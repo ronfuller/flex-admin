fc1953097b894e0085203f7dce0f781884c49c39
Ron Fuller <ron@pacificscreening.com>
2022-02-05 10:37:39 -0700

    Configured Package

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace Psi\FlexAdmin\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,5 @@
 <?php
 
-use VendorName\Skeleton\Tests\TestCase;
+use Psi\FlexAdmin\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
